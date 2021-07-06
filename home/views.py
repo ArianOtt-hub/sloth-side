3a8a4194db60b41a7450ee9700abbcdb472709ff
ottar <arian.ott@thunderlight.de>
2021-07-06 21:09:09 +0200

    v. 1.0.0 First commit done -nav changed -urls

--- home/views.py
+++ home/views.py
@@ -1,11 +1,10 @@
-from django.shortcuts import render
 from django.views.generic import ListView
+
 from . import models
 
 # Create your views here.
 
 class HomeOverview(ListView):
-
     model = models.HomeOverview
     template_name = 'home/overview.html'
     context_object_name = 'homeoverview'
